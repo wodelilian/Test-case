1bcb010cadac114bb8228550768e690d900309e7
故事的小黄花 <73230103+wodelilian@users.noreply.github.com>
2021-11-03 10:30:11 +0800

    Update test_login1.py

--- Suites/test_login1.py
+++ Suites/test_login1.py
@@ -4,9 +4,9 @@ from selenium import webdriver
 def test_login():
     driver = webdriver.Chrome()
     driver.implicitly_wait(10)
-    driver.get('http://47.108.13.13:6983')
-    driver.find_element_by_xpath('//*[@id="userName"]').send_keys('admin')
-    driver.find_element_by_xpath('//input[@class="userPwd"]').send_keys("Ycya!@#123")
+    driver.get('url')
+    driver.find_element_by_xpath('//*[@id="userName"]').send_keys('user')
+    driver.find_element_by_xpath('//input[@class="userPwd"]').send_keys("pwd")
     driver.find_element_by_xpath('//button[@type="button"').click()
 
 
